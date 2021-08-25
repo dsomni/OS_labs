eebb2e4ff27d7bf29b3da6921a453248f0338ecc
Dmitry <pro100pro10010@gmail.com>
2021-08-25 12:04:00 +0300

    Update [week2] ex3

--- week2/ex3.c
+++ week2/ex3.c
@@ -60,10 +60,29 @@ int main(int argc, char *argv[])
     int n;
     sscanf(argv[1], "%d", &n);
 
-    printTriangle(n);
-    printSemiTriangle(n);
-    printArrow(n);
-    printSquare(n);
+    int userInput;
+    printf("Choose a figure type:\n\t(1) Triangle\n\t(2) Semitriangle\n\t(3) Arrow\n\t(4) Square\n");
+    scanf("%d", &userInput);
+
+    switch (userInput)
+    {
+    case 1:
+        printTriangle(n);
+        break;
+    case 2:
+        printSemiTriangle(n);
+        break;
+    case 3:
+        printArrow(n);
+        break;
+    case 4:
+        printSquare(n);
+        break;
+
+    default:
+        printf("Incorrect input");
+        break;
+    }
 
     return 0;
 }
